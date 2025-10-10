88accc8304af7a7d78334d2d03fdfb8473beb0a8
Басс Павел Валерьевич <basspv@sibur.ru>
2025-10-10 04:29:21 +0300

    Add private message

--- src/goga/utils.py
+++ src/goga/utils.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 def get_project_root() -> Path:
     """Путь до корневого каталога проекта"""
-    return Path(__file__).parent.parent
+    return Path(__file__).parent.parent.parent.absolute()
 
 def get_data_directory() -> Path:
     """Путь до каталога с данными"""
-    return get_project_root() / 'data'
+    return get_project_root() / 'data/'
 
 
 def get_images_directory() -> Path:
     """Путь до каталога с изображениями"""
-    return get_data_directory() / 'images'
+    return get_data_directory() / 'images/'
 
