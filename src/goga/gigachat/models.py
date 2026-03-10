7c129340ba84053d294a6b892420971075b7564b
Басс Павел Валерьевич <basspv@sibur.ru>
2026-03-10 11:19:04 +0300

    Change model to Pro

--- src/goga/gigachat/models.py
+++ src/goga/gigachat/models.py
@@ -9,7 +9,7 @@ load_dotenv(find_dotenv())
 def create_gigachat_model():
     """Фабрика моделей GigaChat"""
     model = GigaChat(
-        model='GigaChat-2-Max',
+        model='GigaChat-2-Pro',
         verify_ssl_certs=False,
     )
     return model
