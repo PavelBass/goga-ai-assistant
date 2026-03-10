ec5697024126684a687983659b20678364aa9e1b
Басс Павел Валерьевич <basspv@sibur.ru>
2026-03-11 02:18:34 +0300

    Change model

--- src/goga/gigachat/models.py
+++ src/goga/gigachat/models.py
@@ -9,7 +9,7 @@ load_dotenv(find_dotenv())
 def create_gigachat_model():
     """Фабрика моделей GigaChat"""
     model = GigaChat(
-        model='GigaChat-2-Pro',
+        model='GigaChat-2-Max',
         verify_ssl_certs=False,
     )
     return model
